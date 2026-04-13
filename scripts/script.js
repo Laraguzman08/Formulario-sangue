359cee8a00f7117d9556ef408293f6ebaa8e7e6f
LaraEmiliaGB <lara_guzman@estudante.sesisenai.org.br>
2026-04-13 15:56:25 -0300

    add - array e resultado

--- scripts/script.js
+++ scripts/script.js
@@ -11,18 +11,30 @@ document.getElementById("formDoacao").addEventListener("submit", function(e) {
     let telefone = document.getElementById("telefone").value;
     let cidade = document.getElementById("cidade").value;
     let estado = document.getElementById("estado").value;
+    let doadores = [];
 
+    // validações
     if(nome === "" || email === "" || idade === "" || peso === "" || sangue === "" || 
         telefone === "" || cidade === "" || estado === "") return alert("Todos os campos são obrigatórios!");
-
     if(nome.split(" ").length < 2) return alert("Nome deve conter pelo menos nome e sobrenome!");
-
     if(!email.includes("@")) return alert("Insira um email com formato válido!");
-
     if(idade < 16) return alert("Você precisa ter no mínimo 16 anos para doar sangue.");
-
     if(peso < 50) return alert("O peso mínimo para doar é 50kg.");
-
     if(isNaN(telefone)) return alert("Telefone deve conter apenas números!");
 
+    // armazenando dados no array
+    let doador = {
+        nome: nome, 
+        email: email, 
+        idade: idade, 
+        peso: peso,
+        sangue: sangue,
+        telefone: telefone,
+        cidade: cidade,
+        estado: estado
+    };
+    doadores.push(doador); // adiciona objeto doador dentro do array doadores
+    console.log(doadores);
+
+    document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!❤️";
 })
