226fe29a965b525bc04f10dc6ac100d9cd2cbe6b
LaraEmiliaGB <lara_guzman@estudante.sesisenai.org.br>
2026-04-13 15:10:48 -0300

    add = validação todos os campos

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
-document.getElementById("formDoacao").addEventListener("submit", function(e) { // pega o formulário pelo id
+document.getElementById("formDoacao").addEventListener("submit", function(e) { 
+// pega o formulário pelo id e executa o código quando o form é enviado
     e.preventDefault(); // impede o comportamento padrão do formulário, que seria recarregar a página ao enviar
     
+    // pegando o valor digitado no campo pelo id
     let nome = document.getElementById("nome").value;
     let email = document.getElementById("email").value;
     let idade = document.getElementById("idade").value;
@@ -9,4 +11,12 @@ document.getElementById("formDoacao").addEventListener("submit", function(e) { /
     let telefone = document.getElementById("telefone").value;
     let cidade = document.getElementById("cidade").value;
     let estado = document.getElementById("estado").value;
+
+    let resultado = document.getElementById("resultado");
+
+    if(nome === "" || email === "" || idade === "" || peso === "" || sangue === "" || 
+        telefone === "" || cidade === "" || estado === "") {
+            resultado = alert("Todos os campos são obrigatórios!");
+            return;
+    }
 })
